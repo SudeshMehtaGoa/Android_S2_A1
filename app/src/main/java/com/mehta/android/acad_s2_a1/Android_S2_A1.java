feeb5da9aa3223f88de3b7d284376a677c3c0d0a
Mehta Sudesh <sudeshiazi@gmail.com>
2018-04-03 18:04:31 +0200

    commit

---
+++ app/src/main/java/com/mehta/android/acad_s2_a1/Android_S2_A1.java
@@ -0,0 +1,13 @@
+package com.mehta.android.acad_s2_a1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Android_S2_A1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android__s2__a1);
+    }
+}
